cac763bf4b58db291bfceaa6033b2082d391e2ab
Githubsuer1 <mishrasiddhant560@gmail.com>
2024-08-08 03:03:47 +0530

    header updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,24 +2,69 @@ import { useState } from "react"
 import {NavLink} from "react-router-dom"
 
 const Header = ()=>{
-    const [fullScreen,setFullScreen] = useState(false);
+    const [open,setOpen] = useState(false)
+
+    const handleClick = ()=>{
+        setOpen((prev)=>!prev)
+    }
 
     return (
-        <nav className="bg-gray-800 grid grid-cols-1 sm:grid-cols-2 p-3.5  sm:sticky top-0">
-            <div className="flex items-center" >
-                <NavLink 
-                className="text-white text-md sm:text-xl font-bold" 
-                to='#'>
-                Siddhant Mishra</NavLink>
+        <nav className="bg-gray-800 flex flex-col sm:flex-row justify-between p-3.5  sm:sticky top-0">
+
+            <div className="flex justify-between">
+                <div className="flex items-center" >
+                    <NavLink 
+                    className="text-white text-md sm:text-xl font-bold" 
+                    to='#'>
+                    Siddhant Mishra</NavLink>
+                </div>
+        
+                <div className="sm:hidden grid gap-1 border p-1 rounded" onClick={handleClick}>
+                    <div className="w-7 border-2 bg-white rounded"></div>
+                    <div className="w-7 border-2 bg-white rounded"></div>
+                    <div className="w-7 border-2 bg-white rounded"></div>
+                </div>
             </div>
-    
+            
 
+            <div className={`sm:hidden ${!open ? "hidden" : "grid grid-cols-1 gap-2 mt-2"}`}>
+            <NavLink 
+                    className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
+                    ${ isActive ? 'text-red-500' : "text-white" }`} 
+                    to='/contact'
+                >
+                Connect Me</NavLink>
 
-            <div className={`grid grid-cols-1 sm:grid-cols-4  items-center sm:justify-items-center gap-1.5 mt-2 ` }>
+                
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
-                    ${ isActive ? "text-red-500" : "text-white" }`} 
+                    ${isActive ? "text-red-500" : "text-white" }`} 
+                    to=''
+                >
+                About Me</NavLink>
+
+                <NavLink 
+                    className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
+                    ${isActive ? "text-red-500" : "text-white" }`} 
+                    to='/work'
+                >
+                Project</NavLink>
+
+                <NavLink 
+                    className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
+                    ${isActive ? "text-red-500" : "text-white" }`} 
+                    to='/skills'
+                >
+                Skills</NavLink>
+            </div>
+
+            
+            <div className={`w-[60%] hidden sm:grid grid-cols-4  items-center sm:justify-items-center gap-3 mt-2` }>
+
+                <NavLink 
+                    className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
+                    ${ isActive ? 'text-red-500' : "text-white" }`} 
                     to='/contact'
                 >
                 Connect Me</NavLink>
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
 import About from './Components/About.jsx'
 import Contact from './Components/Contact.jsx'
 import Work from './Components/Work.jsx'
 import Skills from './Components/Skills.jsx'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
@@ -37,6 +37,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+          <RouterProvider router={router} />
   </React.StrictMode>,
 )
