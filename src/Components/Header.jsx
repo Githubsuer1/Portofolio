71f4d2b2653b3f7b28c54f0a8c07c861bb9c8e79
Githubsuer1 <mishrasiddhant560@gmail.com>
2024-08-08 03:20:59 +0530

    header updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,88 +9,83 @@ const Header = ()=>{
     }
 
     return (
+        // navbar of the site
         <nav className="bg-gray-800 flex flex-col sm:flex-row justify-between p-3.5  sm:sticky top-0">
 
+
             <div className="flex justify-between">
+
+                {/* Logo or name */}
                 <div className="flex items-center" >
                     <NavLink 
                     className="text-white text-md sm:text-xl font-bold" 
                     to='#'>
                     Siddhant Mishra</NavLink>
                 </div>
-        
+
+                {/* this clickable div will be hidden for larger device and visible for smaller device */}
                 <div className="sm:hidden grid gap-1 border p-1 rounded" onClick={handleClick}>
                     <div className="w-7 border-2 bg-white rounded"></div>
                     <div className="w-7 border-2 bg-white rounded"></div>
                     <div className="w-7 border-2 bg-white rounded"></div>
                 </div>
+
             </div>
             
-
+            {/* this will be going to show when width will be smaller otherwise will be hidden */}
             <div className={`sm:hidden ${!open ? "hidden" : "grid grid-cols-1 gap-2 mt-2"}`}>
-            <NavLink 
+                <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${ isActive ? 'text-red-500' : "text-white" }`} 
                     to='/contact'
-                >
-                Connect Me</NavLink>
-
-                
+                >Connect Me</NavLink>
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${isActive ? "text-red-500" : "text-white" }`} 
                     to=''
-                >
-                About Me</NavLink>
+                >About Me</NavLink>
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${isActive ? "text-red-500" : "text-white" }`} 
                     to='/work'
-                >
-                Project</NavLink>
+                >Project</NavLink>
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${isActive ? "text-red-500" : "text-white" }`} 
                     to='/skills'
-                >
-                Skills</NavLink>
+                >Skills</NavLink>
             </div>
 
             
+            {/* this will going to show when the width will be larger otherwise will be hidden */}
             <div className={`w-[60%] hidden sm:grid grid-cols-4  items-center sm:justify-items-center gap-3 mt-2` }>
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${ isActive ? 'text-red-500' : "text-white" }`} 
                     to='/contact'
-                >
-                Connect Me</NavLink>
-
-                
+                >Connect Me</NavLink>
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${isActive ? "text-red-500" : "text-white" }`} 
                     to=''
-                >
-                About Me</NavLink>
+                >About Me</NavLink>
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${isActive ? "text-red-500" : "text-white" }`} 
                     to='/work'
-                >
-                Project</NavLink>
+                >Project</NavLink>
 
                 <NavLink 
                     className={({isActive})=>`sm:bg-transparent bg-gray-600 text-white text-md sm:text-lg font-bold px-1.5 rounded 
                     ${isActive ? "text-red-500" : "text-white" }`} 
                     to='/skills'
-                >
-                Skills</NavLink>
+                >Skills</NavLink>
             </div>
         </nav>
     )
