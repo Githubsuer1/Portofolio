81db6fb084531a46b362c5386ee0f63448ca4382
Githubsuer1 <mishrasiddhant560@gmail.com>
2024-08-13 02:58:06 +0530

    Password generator app added

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -49,7 +49,7 @@ const Work = ()=>{
 
             <div className="w-full  max-w-sm">
             <NavLink 
-                    to=''
+                    to='https://githubsuer1.github.io/PasswordGenerator/'
                     className=' bg-white/40 rounded shadow-2xl p-8   box-border flex flex-col gap-3 overflow-hidden'
                 >
                     
